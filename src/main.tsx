3d70e52322e3e8cd8f2d9aeb909e242330bc0922
TOBYhhw <1505994122@qq.com>
2022-11-14 18:56:19 +0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//样式初始化一半放在最前
+import "reset-css"
+//UI框架的样式
+import "./assets/styles/global.scss"
+//组件的样式
+
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
